d21faa04c406be7e4aff87c9bd2cd093a1abc72e
Rajesh Choudhary <101490518+coding-At-GitHub@users.noreply.github.com>
2024-05-24 12:25:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btn = "button";
 const togg = 'toggle';
 const grow = 'growth';
 const yout = 'youtube';
-const altern = 'alterntively';
+const altern = 'alternatively';
 
 growth increment 
 high package 
